eae64ab0923e5b8d135595cd3815367f96f60725
Praveen Puglia <praveenpuglia@gmail.com>
2017-03-30 17:44:53 +0530

    added clipboard copy support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
+import * as Clipboard from 'clipboard/dist/clipboard.min.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  qrCodeApiLink = "https://api.qrserver.com/v1/create-qr-code/?size=185x185&data=http%3A%2F%2Fpraveenpuglia.com"
+export class AppComponent implements OnInit {
+  @ViewChild('copyButton') copyButton: ElementRef;
+
+  qrCodeApiLink = "https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=http%3A%2F%2Fpraveenpuglia.com"
+
+  ngOnInit(){
+    new Clipboard(this.copyButton.nativeElement);
+  }
 }
